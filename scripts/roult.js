55a92267534e03becd5093f8dddb5058cbd93cd2
Folakl <yakubroimot2001@gmail.com>
2025-01-16 02:25:14 -0800

    Push only static files for production

--- scripts/roult.js
+++ scripts/roult.js
@@ -1,24 +1,24 @@
 function brandInput(){
-    window.location.href = "pages/ecommerce.html"
+    window.location.href = "ecommerce.html"
 }
 function navigateTo(page){
     if (page === "signup" ){
-    window.location.href = "index.html"
+    window.location.href = "/index.html"
     }
     else if (page === "createaccount"){
         window.location.href = "pages/login.html"
     }
     else if (page === "forgotPassword"){
-        window.location.href = "pages/reset.html"
+        window.location.href = "/pages/reset.html"
     }
     else if (page === "balance"){
-        window.location.href = "pages/balance.html"
+        window.location.href = "/pages/balance.html"
     }
     else if (page === "login"){
-        window.location.href = "pages/login.html"
+        window.location.href = "/pages/login.html"
     }
     else if (page === "home"){
-        window.location.href = "pages/ecommerce.html"
+        window.location.href = "/pages/ecommerce.html"
     }
     else{
         alert("page not found")
