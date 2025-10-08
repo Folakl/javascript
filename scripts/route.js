110ac3be9dd4508de7b056a892e7a53858367047
Royam <yakubroimot2001@gamil.com>
2025-10-08 14:10:01 +0100

    update

---
+++ scripts/route.js
@@ -0,0 +1,26 @@
+function brandInput(){
+    window.location.href = "/Bank.html"
+}
+function navigateTo(page){
+    if (page === "signup" ){
+    window.location.href = "/javascript/src/pages/Signup.html"
+    }
+    else if (page === "createaccount"){
+        window.location.href = "/javascript/src/pages/login.html"
+    }
+    else if (page === "forgotPassword"){
+        window.location.href = "/javascript/src/pages/reset.html"
+    }
+    else if (page === "balance"){
+        window.location.href = "/javascript/src/pages/balance.html"
+    }
+    else if (page === "login"){
+        window.location.href = "/javascript/src/pages/login.html"
+    }
+    else if (page === "home"){
+        window.location.href = "/javascript/Bank.html"
+    }
+    else{
+        alert("page not found")
+    }
+}
